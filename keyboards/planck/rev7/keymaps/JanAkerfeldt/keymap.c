97a43ec425c9b9b535e0f8b4f54255ba8a5b59db
Jan Åkerfeldt <jan@akerfeldt.net>
2024-05-13 13:01:59 +0200

    added-escae

--- keyboards/planck/rev7/keymaps/JanAkerfeldt/keymap.c
+++ keyboards/planck/rev7/keymaps/JanAkerfeldt/keymap.c
@@ -34,6 +34,7 @@ enum swedish_keycodes {
 };
 
 #define CTL_ESC LCTL_T(KC_ESC)
+#define ALT_ESC LALT_T(KC_ESC)
 #define CTL_BSP LCTL_T(KC_BSPC)
 #define GUI_GRV LGUI(KC_GRV)
 
@@ -45,13 +46,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ARING,
     CTL_BSP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    OUML,    AUML,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-    FUNC,    CTL_ESC, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_ENT,  KC_RALT, KC_DOWN, KC_RGHT),
+    FUNC,    CTL_ESC, ALT_ESC, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_ENT,  KC_RALT, KC_DOWN, KC_RGHT),
 
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    OUML,    ARING,
     CTL_BSP, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    AUML,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-    FUNC,    CTL_ESC, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_ENT,  KC_RALT, KC_DOWN, KC_RGHT),
+    FUNC,    CTL_ESC, ALT_ESC, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_ENT,  KC_RALT, KC_DOWN, KC_RGHT),
 
 [_LOWER] = LAYOUT_planck_grid(
    KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
